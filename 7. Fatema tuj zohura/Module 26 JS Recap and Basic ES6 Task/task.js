f96050c70054a196b9e854a8c5e567a1da8c24b0
Fatema <fatematz486@gmail.com>
2026-02-26 16:18:06 +0600

    Fatema

--- 7. Fatema tuj zohura/Module 26 JS Recap and Basic ES6 Task/task.js
+++ 7. Fatema tuj zohura/Module 26 JS Recap and Basic ES6 Task/task.js
@@ -6,6 +6,14 @@ if block এর ভিতরে console.log করে দেখাও কোন�
 */
 // Ans:
 
+function add () {
+    
+  var n = 10;
+}
+// console.log( n )
+
+
+
 /* 
 ? Qs: 2
 calculateTotal নামে একটি function বানাও।
