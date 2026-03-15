b1ae50f71de7cf0b8f71d23ac2aa02835b2fc603
tirthosarkar <tirthosarkar1@gmail.com>
2026-03-15 17:54:32 +0600

    added Tirtho Sarkar

---
+++ 8.Tirtho Sarkar/Module 27 (advanced) More ES6 Task/day4Task.js
@@ -0,0 +1,141 @@
+/* =========================================
+        🚀 DAY 4 — JS Freestyle Problems
+        (Method free — map/filter/reduce তুমি ঠিক করবে)
+========================================= */
+
+/* 
+? Problem 1
+একটা array আছে, সব element cube করো। 
+const numbers = [2, 3, 4, 5];
+Expected Output: [8, 27, 64, 125]
+*/
+//* Ans:
+
+/* 
+? Problem 2
+50 এর কম mark থাকা students বের করো। 
+const students = [
+  { name: "Ali", mark: 45 },
+  { name: "Babu", mark: 75 },
+  { name: "Chotu", mark: 30 },
+  { name: "Dipu", mark: 90 }
+];
+Expected Output: [{ name: "Ali", mark: 45 }, { name: "Chotu", mark: 30 }]
+*/
+//* Ans:
+
+/* 
+? Problem 3
+সব numbers যোগ করো। 
+const nums = [10, 20, 30, 40];
+Expected Output: 100
+*/
+//* Ans:
+
+/* 
+? Problem 4
+প্রথম student যার mark > 80 খুঁজে বের করো। 
+const students = [
+  { name: "Ali", mark: 45 },
+  { name: "Babu", mark: 75 },
+  { name: "Chotu", mark: 85 },
+  { name: "Dipu", mark: 90 }
+];
+Expected Output: { name: "Chotu", mark: 85 }
+*/
+//* Ans:
+
+/* 
+Problem 5
+index সহ fruits print করো। 
+const fruits = ["Mango", "Banana", "Apple", "Orange"];
+Expected Output:
+0 - Mango
+1 - Banana
+2 - Apple
+3 - Orange
+*/
+//* Ans:
+
+/* 
+?Problem 6
+mark > 50 student এর শুধু নাম বের করো। 
+const students = [
+  { name: "Ali", mark: 45 },
+  { name: "Babu", mark: 75 },
+  { name: "Chotu", mark: 85 },
+  { name: "Dipu", mark: 30 }
+];
+Expected Output: ["Babu", "Chotu"]
+*/
+//* Ans:
+
+/* 
+?Problem 7
+সব element multiply করো। 
+const nums = [1, 2, 3, 4, 5];
+Expected Output: 120
+*/
+//* Ans:
+
+/* 
+? 8
+Object থেকে name, email, city বের করো। 
+const user = {
+  id: 101,
+  name: "Rahim",
+  email: "rahim@gmail.com",
+  address: { city: "Dhaka", country: "Bangladesh" }
+};
+Expected Output: Rahim rahim@gmail.com Dhaka
+*/
+//* Ans:
+
+/* 
+?Problem 9
+Variable hoisting output কি হবে লিখে দেখো। 
+console.log(aVar);
+var aVar = 50;
+
+console.log(bVar);
+let bVar = 30;
+Expected Output: ? 
+*/
+//* Ans:
+
+/* 
+?Problem 10
+Closure counter তৈরি করো, প্রত্যেকবার call করলে +1 হবে। 
+function counter() {
+  let count = 0;
+  return function() {
+    // তোমার code এখানে
+  }
+}
+const myCounter = counter();
+myCounter(); // 1
+myCounter(); // 2
+myCounter(); // 3
+*/
+//* Ans:
+
+/* 
+? Problem 11
+even numbers এর square যোগ করো। 
+const numbers = [1, 2, 3, 4, 5, 6];
+Expected Output: 56
+*/
+//* Ans:
+
+/* 
+? Problem 12
+price > 500 object এর total quantity বের করো। 
+const cart = [
+  { name: "Shirt", price: 500, quantity: 2 },
+  { name: "Pant", price: 800, quantity: 3 },
+  { name: "Shoes", price: 1500, quantity: 1 },
+  { name: "Cap", price: 300, quantity: 5 }
+];
+Expected Output: 4
+*/
+//* Ans:
